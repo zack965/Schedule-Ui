cad5a5734bc2fd66b8666fe3c13ac8e6cd44ea86
zack965 <67732045+zack965@users.noreply.github.com>
2022-01-24 09:49:15 +0100

    starting events

---
+++ src/Data/EventsData.js
@@ -0,0 +1,16 @@
+export let Events = [
+  {
+    id: 1,
+    resource: "R1",
+    start: "2022-01-04T00:00:00",
+    end: "2022-01-08T00:00:00",
+    text: "Event 1",
+  },
+  {
+    id: 2,
+    resource: "R1",
+    start: "2022-01-12T00:00:00",
+    end: "2022-01-16T00:00:00",
+    text: "Event 2",
+  },
+];
